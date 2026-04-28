ccda0575830287097aa59b9b622e4abee24a301c
Fahd36631 <Fahd36631@gmail.com>
2026-04-28 09:21:50 +0300

    Fix FizzBuzz script to run as executable and print up to provided argument

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 """FizzBuzz challenge fix."""
 
+import sys
 
-def fizzbuzz():
-    """Print numbers from 1 to 100 with FizzBuzz rules."""
-    for i in range(1, 101):
+
+def fizzbuzz(limit=100):
+    """Print numbers from 1 to limit with FizzBuzz rules."""
+    output = []
+    for i in range(1, limit + 1):
         if i % 15 == 0:
-            print("FizzBuzz", end=" ")
+            output.append("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            output.append("Fizz")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            output.append("Buzz")
         else:
-            print(i, end=" ")
+            output.append(str(i))
+    print(" ".join(output))
 
 
 if __name__ == "__main__":
-    fizzbuzz()
-    print()
+    if len(sys.argv) > 1:
+        fizzbuzz(int(sys.argv[1]))
+    else:
+        fizzbuzz()
